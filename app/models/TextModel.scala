a49abf1e005427a7239b178d04f2a79fd00e26ff
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-08-14 22:48:00 +0530

    git status

--- app/models/TextModel.scala
+++ app/models/TextModel.scala
@@ -12,8 +12,7 @@ case class TextModel(
                       //language: Option[String],
                       tags: List[String],
                       by: Option[String],
-                      user: Option[String],
-                      verified: Option[Boolean]
+                      user: Option[String]
                     )
 
 object TextModel {
@@ -24,8 +23,7 @@ case class TextRequestModel(
                       text: String,
                       tags: List[String],
                       by: Option[String],
-                      user: Option[String],
-                      verified: Option[Boolean]
+                      user: Option[String]
                     )
 
 object TextRequestModel {
@@ -33,12 +31,12 @@ object TextRequestModel {
 
   def getTextId(text: String): String = {
     val strippedText = text.replaceAll("[^a-zA-Z\\d\\s:]", "").replaceAll(" ", "-").toLowerCase
-    val stripLength = if(strippedText.length > 100) 100 else strippedText.length
+    val stripLength = if(strippedText.length > 120) 120 else strippedText.length
     strippedText.substring(0, stripLength)
   }
 
   def toTextModel(textRequestModel: TextRequestModel): TextModel = {
-    TextModel(getTextId(textRequestModel.text), textRequestModel.text, 0, textRequestModel.tags, textRequestModel.by, textRequestModel.user, None)
+    TextModel(getTextId(textRequestModel.text), textRequestModel.text, 0, textRequestModel.tags, textRequestModel.by, textRequestModel.user)
   }
 
 }
